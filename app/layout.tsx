d02663a0089ee4b13e14037597cbe411351fa9f1
Your Name <you@example.com>
2025-03-24 05:44:59 +0300

    GTM added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "ECITech - Programming The Future of Your Business",
@@ -17,7 +18,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="overscroll-none h-full">{children}</body>
+      <head>
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KMRP9WR8');
+          `}
+        </Script>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="" />
+        <link rel="icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+        <link rel="manifest" href="/img/site.webmanifest" />
+        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+      <body className="overscroll-none h-full">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMRP9WR8"
+            height="0" width="0" 
+            style={{ display: "none", visibility: "hidden" }}>
+          </iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
   return (
     <>
       <Head>
+
         <title>ECITech</title>
         <link rel="icon" href="#" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
