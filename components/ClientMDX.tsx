4120dfd82a6e99293f93942e847a68223d7df38c
Marat <ulakaev05@gmail.com>
2025-04-25 15:00:35 +0300

    tables view edit

--- components/ClientMDX.tsx
+++ components/ClientMDX.tsx
@@ -11,9 +11,13 @@ export default function ClientMDX({ images, ...mdxProps }: Props) {
     <MDXRemote
       {...mdxProps}
       components={{
-        table: (p) => <table className="w-full border-collapse mb-6" {...p}/>,
-        th:    (p) => <th className="border px-4 py-2 bg-gray-100 text-left" {...p}/>,
-        td:    (p) => <td className="border px-4 py-2" {...p}/>,
+        table: (p) => (
+          <div className="overflow-x-auto sm:overflow-x-scroll scrollbar-hide my-6">
+            <table className="w-full rounded-xl border-collapse overflow-hidden" {...p}/>
+          </div>
+        ),
+        th:    (p) => <th className="px-4 py-2 bg-gray-700 text-left" {...p}/>,
+        td:    (p) => <td className="px-4 py-2" {...p}/>,
         DynamicImage: ({ index }: { index: number }) => (
           <ImageByIndex url={images[index]} />
         ),
