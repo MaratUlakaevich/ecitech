2db5c43e579e065a5ff47fcd4cfb5f5d01dbb89f
Marat <ulakaev05@gmail.com>
2025-04-26 14:10:54 +0300

    sitemap updated

--- app/sitemap/route.ts
+++ app/sitemap/route.ts
@@ -17,19 +17,26 @@ export async function GET(): Promise<Response> {
 
   const {data: articles} = await getAllArticles(); // должен возвращать массив статей
 
-  const dynamicBlogRoutes = articles.map((article: Article) => `/blog/${article.slug}`);
+  const dynamicBlogRoutes = articles.map((article: Article) => ({
+    url: `/blog/${article.slug}`,
+    lastmod: article.updatedAt,
+  }));
 
-  const allRoutes = [...staticRoutes, ...dynamicBlogRoutes];
+  const allRoutes = [
+    ...staticRoutes.map((route) => ({ url: route, lastmod: new Date().toISOString() })), 
+    ...dynamicBlogRoutes
+  ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${allRoutes
       .map(
-        (route) => `
+        ({ url, lastmod }) => `
       <url>
-        <loc>${baseUrl}${route}</loc>
+        <loc>${baseUrl}${url}</loc>
+        <lastmod>${lastmod}</lastmod>
         <changefreq>weekly</changefreq>
-        <priority>${route === "/" ? "1.0" : "0.8"}</priority>
+        <priority>${url === "/" ? "1.0" : "0.8"}</priority>
       </url>`
       )
       .join("")}
